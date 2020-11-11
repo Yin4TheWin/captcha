799c1fdfa19b5ddd66466e5bfc2a42dbb046ab22
Franklin Yin <36430386+Yin4TheWin@users.noreply.github.com>
2020-11-11 15:51:59 -0500

    Update variables.js

--- variables.js
+++ variables.js
@@ -4,7 +4,7 @@ const discord_guild_id = '';
 const discord_verified_role = '';
 
 // reCAPTCHA v2 secret key
-const recaptcha_secret_key = '';
+const recaptcha_secret_key = '6LdB5OEZAAAAAKMAixU3EZ-__zPoQ_xqcJ1KWjbD';
 
 // Domain
 const domain = ''; // Leave empty for localhost
