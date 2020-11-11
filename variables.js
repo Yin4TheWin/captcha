7b4cfc98d82bbb8f09f7d61b30c9eb7ffdfbc302
Franklin Yin <36430386+Yin4TheWin@users.noreply.github.com>
2020-11-11 15:54:38 -0500

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,7 +1,7 @@
 // Discord info
-const discord_bot_token = '';
-const discord_guild_id = '';
-const discord_verified_role = '';
+const discord_bot_token = 'Nzc2MTcyOTg5ODIwOTYwODQw.X6xBbQ.Yd9JtAg5G_-B-o_ZK9ik9a9p_Gc';
+const discord_guild_id = '776188087188717610';
+const discord_verified_role = 'verified';
 
 // reCAPTCHA v2 secret key
 const recaptcha_secret_key = '6LdB5OEZAAAAAKMAixU3EZ-__zPoQ_xqcJ1KWjbD';
